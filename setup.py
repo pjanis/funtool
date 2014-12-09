95a790ed458fa6bae92a3c165537cd4065c16171
Philip Janisiewicz <pjanisiewicz@gmail.com>
2014-12-09 18:03:41 +0100

    updated setup files

--- setup.py
+++ setup.py
@@ -2,9 +2,22 @@ from setuptools import setup
 
 setup(name='funtool',
         version='0.0.75',
-        description='FUN Tool engine to process an analysis',
+        url='https://github.com/pjanis/funtool',
+        description='FUN Tool engine: Used process an analysis',
         author='Active Learning Lab',
         author_email='pjanisiewicz@gmail.com',
         license='MIT',
         packages=['funtool'],
+        classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.2',
+            'Programming Language :: Python :: 3.3',
+            'Programming Language :: Python :: 3.4'
+        ],
+        install_requires=[
+            'PyYAML'
+        ],
         zip_safe=False)
